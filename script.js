54258d52a489caf024b7e77179a7250ce54aa600
Peemapos.At <79019396+Peemapos@users.noreply.github.com>
2024-06-05 22:54:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+const formEle = document.querySelector('#searchForm')
+const inputEle = document.querySelector('input')
+const searchBtn = document.querySelector('#searchResult')
+const resetBtn = document.querySelector('#resetBtn')
+const searchResult = document.querySelector('.Result')
+const containerResult = document.querySelector('.containerResult')
+const dropdownEle = document.querySelector('#dropDown')
+
+const getDropdown = async () =>{
+    const resAll = await axios.get('https://pokeapi.co/api/v2/pokemon/')
+        for (const lists of resAll.data.results) {
+            const newLink = document.createElement('option')
+            newLink.innerText += lists.name
+            newLink.setAttribute('value', lists.name)
+            dropdownEle.appendChild(newLink)
+        }
+}
+getDropdown()
+
+dropdownEle.addEventListener('change', function (e) {
+    searchResult.innerHTML='';
+    getPokemonDetails(dropdownEle.value)
+})
+
+
+searchBtn.addEventListener('click', function (e) {
+    e.preventDefault()
+    let search = formEle.elements.query.value //ค่าที่พิมพ​์ input ไป
+    searchResult.innerHTML='';
+    getPokemonDetails(search)
+
+})
+
+
+const getPokemonDetails = async (search) => {
+    try {
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${search}`);
+        renderType(res.data)
+        inputEle.value=''
+
+    } catch (err) {
+        alert("Invalid Name or ID")
+        console.log(err);
+        inputEle.value=''
+    }
+}
+
+const renderType = (data) => {
+    const num = 0;
+    if (data.name) {
+        const containEle = document.createElement('div')
+        searchResult.appendChild(containEle)
+        const imgEle = document.createElement('img')
+        imgEle.src = `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${data.id}.svg`
+        containEle.appendChild(imgEle)
+        const divEleName = document.createElement('div')
+        divEleName.innerText += data.name
+        containEle.appendChild(divEleName)
+        for (let item of data.types) {
+            const divEleType = document.createElement('div')
+            divEleType.innerText += `Type:${item.type.name}`
+            containEle.appendChild(divEleType)
+
+    }
+}
+
+}
